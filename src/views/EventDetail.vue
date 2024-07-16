cd90244c1c78dfb0fef93e5a47b6006b60be5054
IngM5058 <153575241+IngM5058@users.noreply.github.com>
2024-07-16 15:25:42 +0700

    1.1

---
+++ src/views/EventDetail.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import type { EventItem } from '@/type'
+const event: Ref<EventItem | null> = ref(null)
+
+</script>
+
+<template>
+    <div v-if="event">
+        <h1>{{ event.title }}</h1>
+        <p>{{ event.time }} on {{ event.location }}</p>
+        <p>{{ event.description }}</p>
+    </div>
+</template>
