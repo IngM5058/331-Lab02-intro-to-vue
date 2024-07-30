0257e33c34cd9847ae2f4394df327d29d83715ed
IngM5058 <153575241+IngM5058@users.noreply.github.com>
2024-07-30 22:48:38 +0700

    till 2.4

--- 331-intro-to-vue/src/services/EventService.ts
+++ 331-intro-to-vue/src/services/EventService.ts
@@ -10,8 +10,8 @@ const apiClient = axios.create({
 })
 
 export default {
-  getEvents() {
-    return apiClient.get('/events')
+  getEvents(perpage: number, page: number) {
+    return apiClient.get('/events?_limit=' + perpage + '&_page=' + page)
   },
   getEvent(id: number) {
     return apiClient.get('/events/' + id)
